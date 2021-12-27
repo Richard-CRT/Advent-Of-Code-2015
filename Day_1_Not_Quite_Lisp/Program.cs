93ce855951f47460be5fda94aacd3eb8b4f608df
Richard Carter <Richard-CRT@users.noreply.github.com>
2021-12-27 12:23:26 +0000

    Add project files.

---
+++ Day_1_Not_Quite_Lisp/Program.cs
@@ -0,0 +1,38 @@
+﻿using AdventOfCodeUtilities;
+
+void P1(string directions)
+{
+    int floor = 0;
+    foreach (char c in directions)
+    {
+        if (c == '(')
+            floor++;
+        else if (c == ')')
+            floor--;
+    }
+    Console.WriteLine(floor);
+    Console.ReadLine();
+}
+
+void P2(string directions)
+{
+    int floor = 0;
+    int i;
+    for (i =0; i < directions.Length;i++)
+    {
+        char c = directions[i];
+        if (c == '(')
+            floor++;
+        else if (c == ')')
+            floor--;
+        if (floor == -1)
+            break;
+    }
+    Console.WriteLine(i+1);
+    Console.ReadLine();
+}
+
+List<string> inputList = AoCUtilities.GetInputLines();
+
+P1(inputList[0]);
+P2(inputList[0]);
