fb4e0078cd171ac80d5ed6f0c1b43eb447f11f0a
Richard Carter <Richard-CRT@users.noreply.github.com>
2021-12-27 13:11:14 +0000

    Day 5 Solution

---
+++ Day_5_Doesn't_He_Have_Intern_Elves_For_This/Program.cs
@@ -0,0 +1,76 @@
+﻿using AdventOfCodeUtilities;
+
+void P1(List<string> strings)
+{
+    int niceStrings = 0;
+    foreach (string s in strings)
+    {
+        int vowelCount = 0;
+        bool repeatedC = false;
+        bool forbiddenSequence = false;
+        char lastC = ' ';
+        foreach (char c in s)
+        {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                vowelCount++;
+            if (lastC == c)
+                repeatedC = true;
+            if ((lastC == 'a' && c == 'b') || (lastC == 'c' && c == 'd') || (lastC == 'p' && c == 'q') || (lastC == 'x' && c == 'y'))
+            {
+                forbiddenSequence = true;
+                break;
+            }
+            lastC = c;
+        }
+        if (vowelCount >= 3 && repeatedC && !forbiddenSequence)
+            niceStrings++;
+    }
+    Console.WriteLine(niceStrings);
+    Console.ReadLine();
+}
+
+void P2(List<string> strings)
+{
+    int niceStrings = 0;
+    foreach (string s in strings)
+    {
+        bool repeats = false;
+        bool pair = false;
+        for (int i = 0; i < s.Length; i++)
+        {
+            char c = s[i];
+
+            if (i >= 2 && c == s[i - 2])
+                repeats = true;
+
+            if (i < s.Length - 1 && !pair)
+            {
+                int j = i + 1;
+
+                for (int x = 0; x < s.Length - 1; x++)
+                {
+                    int y = x + 1;
+
+                    if (y < i || x > j)
+                    {
+                        if (s[i] == s[x] && s[j] == s[y])
+                        {
+                            pair = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (repeats && pair)
+            niceStrings++;
+    }
+    Console.WriteLine(niceStrings);
+    Console.ReadLine();
+}
+
+List<string> inputList = AoCUtilities.GetInputLines();
+
+P1(inputList);
+P2(inputList);
