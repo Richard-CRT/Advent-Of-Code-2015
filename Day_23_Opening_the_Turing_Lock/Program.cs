5023e5f424c6fae6ba12eaae0955b6705ba4e6e4
Richard Carter <Richard-CRT@users.noreply.github.com>
2022-12-04 14:54:13 +0000

    Day 23 & 24 Solutions

---
+++ Day_23_Opening_the_Turing_Lock/Program.cs
@@ -0,0 +1,109 @@
+﻿using AdventOfCodeUtilities;
+
+List<string> inputList = AoCUtilities.GetInputLines();
+List<Instruction> Instructions = inputList.Select(x => new Instruction(x)).ToList();
+
+void P1()
+{
+    int pc = 0;
+    Dictionary<char, int> file = new Dictionary<char, int>() { { 'a', 0 }, { 'b', 0 } };
+
+    while (pc < Instructions.Count)
+    {
+         Instructions[pc].Execute(ref pc, file);
+    }
+
+    Console.WriteLine(file['b']);
+    Console.ReadLine();
+}
+
+void P2()
+{
+    int pc = 0;
+    Dictionary<char, int> file = new Dictionary<char, int>() { { 'a', 1 }, { 'b', 0 } };
+
+    while (pc < Instructions.Count)
+    {
+        Instructions[pc].Execute(ref pc, file);
+    }
+
+    Console.WriteLine(file['b']);
+    Console.ReadLine();
+}
+
+P1();
+P2();
+
+public enum OpCode
+{
+    hlf,
+    tpl,
+    inc,
+    jmp,
+    jie,
+    jio
+}
+
+public class Instruction
+{
+    public OpCode OpCode { get; set; }
+    public char Operand = ' ';
+    public int Offset = 0;
+    public Instruction(string s)
+    {
+        string[] split = s.Split(' ');
+        switch (split[0])
+        {
+            case "hlf":
+                OpCode = OpCode.hlf; break;
+            case "tpl":
+                OpCode = OpCode.tpl; break;
+            case "inc":
+                OpCode = OpCode.inc; break;
+            case "jmp":
+                OpCode = OpCode.jmp; break;
+            case "jie":
+                OpCode = OpCode.jie; break;
+            case "jio":
+                OpCode = OpCode.jio; break;
+            default:
+                throw new NotImplementedException();
+        }
+
+        switch (OpCode)
+        {
+            case OpCode.hlf:
+            case OpCode.tpl:
+            case OpCode.inc:
+                Operand = split[1][0]; break;
+            case OpCode.jmp:
+                Offset = int.Parse(split[1]); break;
+            case OpCode.jie:
+            case OpCode.jio:
+                Operand = split[1][0];
+                Offset = int.Parse(split[2]);
+                break;
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    public void Execute(ref int pc, Dictionary<char, int> file)
+    {
+        switch (OpCode)
+        {
+            case OpCode.hlf:
+                file[Operand] /= 2; pc++; break;
+            case OpCode.tpl:
+                file[Operand] *= 3; pc++; break;
+            case OpCode.inc:
+                file[Operand] += 1; pc++; break;
+            case OpCode.jmp:
+                pc += Offset; break;
+            case OpCode.jie:
+                pc += file[Operand] % 2 == 0 ? Offset : 1; break;
+            case OpCode.jio:
+                pc += file[Operand] == 1 ? Offset : 1; break;
+        }
+    }
+}
---
+++ Day_24_It_Hangs_in_the_Balance/Program.cs
@@ -0,0 +1,107 @@
+﻿using AdventOfCodeUtilities;
+using System.Linq;
+
+List<List<int>>? foo(int target, List<int> options, int startIndex, int length, int depth = 0)
+{
+    List<List<int>>? ret = null;
+    for (int chosenOptionIndex = startIndex; chosenOptionIndex < options.Count - length + 1; chosenOptionIndex++)
+    {
+        int chosenOption = options[chosenOptionIndex];
+        /*
+        for (int i = 0; i < depth; i++)
+            Console.Write(" ");
+        Console.WriteLine(chosenOption);
+        */
+        if (length == 1)
+        {
+            if (target == chosenOption)
+            {
+                if (ret is null)
+                    ret = new List<List<int>>();
+                ret.Add(new List<int> { chosenOption });
+            }
+        }
+        else
+        {
+            List<List<int>>? val = foo(target - chosenOption, options, chosenOptionIndex + 1, length - 1, depth + 1);
+            if (val != null)
+            {
+                foreach (List<int> l in val)
+                {
+                    l.Insert(0, chosenOption);
+                    if (ret is null)
+                        ret = new List<List<int>>();
+                    ret.Add(l);
+                }
+            }
+        }
+    }
+    return ret;
+}
+
+List<string> inputList = AoCUtilities.GetInputLines();
+List<int> PackageWeights = inputList.Select(x => int.Parse(x)).ToList();
+int totalWeight = PackageWeights.Sum();
+
+void P1()
+{
+    int targetWeight = totalWeight / 3;
+
+    List<List<int>>? fewestCombinations = null;
+    for (int i = 1; i < PackageWeights.Count; i++)
+    {
+        fewestCombinations = foo(targetWeight, PackageWeights, 0, i);
+        if (fewestCombinations != null)
+            break;
+    }
+
+    if (fewestCombinations != null)
+    {
+        Int64 minQuantumEntanglement = Int64.MaxValue;
+        foreach (List<int> combination in fewestCombinations)
+        {
+            Int64 quantumEntanglement = 1;
+            foreach (int val in combination)
+                quantumEntanglement *= val;
+            if (quantumEntanglement < minQuantumEntanglement)
+                minQuantumEntanglement = quantumEntanglement;
+        }
+        Console.WriteLine(minQuantumEntanglement);
+    }
+    else
+        Console.WriteLine("null");
+    Console.ReadLine();
+}
+
+void P2()
+{
+    int targetWeight = totalWeight / 4;
+
+    List<List<int>>? fewestCombinations = null;
+    for (int i = 1; i < PackageWeights.Count; i++)
+    {
+        fewestCombinations = foo(targetWeight, PackageWeights, 0, i);
+        if (fewestCombinations != null)
+            break;
+    }
+
+    if (fewestCombinations != null)
+    {
+        Int64 minQuantumEntanglement = Int64.MaxValue;
+        foreach (List<int> combination in fewestCombinations)
+        {
+            Int64 quantumEntanglement = 1;
+            foreach (int val in combination)
+                quantumEntanglement *= val;
+            if (quantumEntanglement < minQuantumEntanglement)
+                minQuantumEntanglement = quantumEntanglement;
+        }
+        Console.WriteLine(minQuantumEntanglement);
+    }
+    else
+        Console.WriteLine("null");
+    Console.ReadLine();
+}
+
+P1();
+P2();
